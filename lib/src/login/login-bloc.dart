83e6aaa3d11d943ab6d5071ba0819f7624b0462b
Amélia de Almeida Guerreiro <ameliadag96@gmail.com>
2019-09-25 23:23:38 -0300

    feito bloco de animação girando

--- lib/src/login/login-bloc.dart
+++ lib/src/login/login-bloc.dart
@@ -1,6 +1,17 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 
 class LoginBloc extends BlocBase{
+  onClickFacebook(){
+
+  }
+
+  onClickGoogle(){
+    
+  }
+
+  onClickTelefone(){
+    
+  }
   @override
   void dispose() {
     // TODO: implement dispose
--- lib/src/login/login-widget.dart
+++ lib/src/login/login-widget.dart
@@ -1,38 +1,77 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mut/src/login/login-bloc.dart';
-
 
 class LoginWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<LoginBloc>(
-      bloc: LoginBloc(),  
-      child: _LoginContent(),
-      
+      bloc: LoginBloc(),
+      child: Material(
+              child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Transgender_Pride_flag.svg/1280px-Transgender_Pride_flag.svg.png", fit: BoxFit.cover),
+            Container(color: Colors.grey[700].withOpacity(0.1),),
+            _LoginContent(),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class _LoginContent extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-
-    _botoes(){
+class _LoginContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _botoes() {
       return Column(
         children: <Widget>[
-          RaisedButton.icon(icon: Icon(Icons.phone), 
-          label: Text("Login com Telefone"), 
-          onPressed: () {},)
+          RaisedButton.icon(
+            color: Colors.green,
+            textColor: Colors.white,
+            icon: Icon(Icons.phone),
+            label: Text("Login com Telefone"),
+            onPressed: () {},
+          ),
+          RaisedButton.icon(
+            color: Colors.red,
+            textColor: Colors.white,
+            icon: Icon(FontAwesomeIcons.google),
+            label: Text("Login com Google    "),
+            onPressed: () {},
+          ),
+          RaisedButton.icon(
+            color: Colors.blue,
+            textColor: Colors.white,
+            icon: Icon(FontAwesomeIcons.facebookF),
+            label: Text("Login com Facebook"),
+            onPressed: () {},
+          ),
         ],
       );
     }
-    return Material(
-      child: Column(
+
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          FlutterLogo(size: 72,),
+          FlutterLogo(
+            size: 72,
+          ),
+          Container(
+            height: 120,
+          ),
+          AnimatedCrossFade(
+            firstChild: _botoes(), 
+            secondChild: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(),
+            ), 
+            duration: Duration(milliseconds: 500),
+            crossFadeState: CrossFadeState.showSecond,
+          ),
         ],
-      )
     );
   }
 }
